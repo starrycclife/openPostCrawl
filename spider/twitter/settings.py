dab7a65b8ba9a8f1d8543a224a699ff662e42d60
huyong <nghuyong@163.com>
2018-05-15 23:56:07 +0800

    fix

--- spider/twitter/settings.py
+++ spider/twitter/settings.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # encoding: utf-8
-ConsumerKey = 'k3Wrz69bi5UHpZM75ZDj2j0d3'
-ConsumerSecret = 'WrgLXt7AD4Krm4AwD26pIeVEQcpzN9CQxbTKsH1Z7tFx4poIYL'
-AccessToken = '995938523973935104-yLQnMnZ4GaNg0LahqpNXZjD8vt7DIkj'
-AccessTokenSecret = 'Ziu6fLqDtwXdFUZjAVMNl5GFk9bHWN4J9gb97Z2dQTVb6'
+ConsumerKey = ''
+ConsumerSecret = ''
+AccessToken = ''
+AccessTokenSecret = ''
