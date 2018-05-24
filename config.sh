12b0af1be24898f90458ee523a3c0ff3056b2029
huyong <nghuyong@163.com>
2018-05-24 13:45:15 +0800

    fix

--- config.sh
+++ config.sh
@@ -1,8 +1,2 @@
 #!/usr/bin/env bash
 apt-get install mongodb
-mkdir spider/weibo/log
-mkdir spider/facebook/log
-mkdir spider/twitter/log
-mkdir spider/youtube/log
-mkdir web/server/static/youtube_videos
-# c_user=100026187121261; xs=19%3ABn7NOVf4AeVnng%3A2%3A1526379440%3A-1%3A-1;
